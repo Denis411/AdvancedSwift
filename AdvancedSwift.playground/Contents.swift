969102aaea16660ed4e261b8f6e3196e5a64163e
Dennis <dnsljnv411@gmail.com>
2022-10-18 22:46:22 +0700

    Add NSObject

--- AdvancedSwift.playground/Contents.swift
+++ AdvancedSwift.playground/Contents.swift
@@ -1,4 +1,5 @@
 import Foundation
+import UIKit
 
 let range: ClosedRange<Int> = 1...3
 let rangeOne = 1...3
@@ -16,5 +17,21 @@ extension BaseClass {
     
     @objc func youCanOverrideMe() {
 //     you can override it because you use a message dispatch here. You do not need you use @objc in parents cuz it is interfered
+    }
+}
+
+class SomeNSClass: NSObject {
+    final func staticFoo() {
+//      final makes this method static dispathc
+    }
+    
+    @objc func messageFoo() {
+//      @objc makes it message dispatch
+    }
+}
+
+extension SomeNSClass {
+    func messageFooTwo() {
+//     its always message dispatch in extensions of NSObject
     }
 }
