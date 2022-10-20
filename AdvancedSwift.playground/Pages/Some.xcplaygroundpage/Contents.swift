f52ef04d7bdd7d49deb0fd346bf6ac3d4a2d1b03
Dennis <dnsljnv411@gmail.com>
2022-10-21 00:45:12 +0700

    Add failable init

--- AdvancedSwift.playground/Pages/Some.xcplaygroundpage/Contents.swift
+++ AdvancedSwift.playground/Pages/Some.xcplaygroundpage/Contents.swift
@@ -3,3 +3,16 @@ import Foundation
 var array = [Int]()
 array.reserveCapacity(10)
 array.removeAll(keepingCapacity: true)
+
+// Failable init can return nil
+struct FailabelStruct {
+    let num: Int
+    
+    init?(num: Int) {
+        guard num % 2 == 0 else {
+            return nil
+        }
+        
+        self.num = num
+    }
+}
