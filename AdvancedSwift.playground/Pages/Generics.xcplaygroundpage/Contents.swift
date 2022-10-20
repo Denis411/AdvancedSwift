9f4f4890763b70778ca9b5fc296244e72b833179
Dennis <dnsljnv411@gmail.com>
2022-10-20 11:08:37 +0700

    Add group for additional group

---
+++ AdvancedSwift.playground/Pages/Generics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+// Generic aka Parametric polymorphisme
+let anyArray: [Any] = [1,2,3]
+let newAnyArray = anyArray as! [Int]
+// - casting
+// - time on a type lookup
+// - might have another type in
+
+let genericArray: Array<Int> = [1,2,3]
+
+func addNums<Num: Numeric>(_ a: Num, _ b: Num) -> Num {
+    a + b
+}
+
+func addNumsTwo<Num>(_ a: Num, _ b: Num) -> Num where Num: Numeric {
+    a + b
+}
+
+protocol SomeElement {
+    var id: String { get set }
+}
+
+class GenericClass<T: SomeElement> { }
+struct GenericStruct<T: SomeElement> { }
+enum GenericEnum<T: SomeElement> { }
+
+// MARK: -- Associated type
+
+protocol AssociatedProtocol {
+    associatedtype NumericType: Numeric
+}
+
+class TypealiasClass: AssociatedProtocol {
+    typealias NumericType = Int
+}
+
+struct TypealiasStruct: AssociatedProtocol {
+    typealias NumericType = Int
+}
+
+enum TypealiasEnum: AssociatedProtocol {
+    typealias NumericType = Int
+}
+
+
---
+++ AdvancedSwift.playground/Pages/Some.xcplaygroundpage/Contents.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+var array = [Int]()
+array.reserveCapacity(10)
+array.removeAll(keepingCapacity: true)
