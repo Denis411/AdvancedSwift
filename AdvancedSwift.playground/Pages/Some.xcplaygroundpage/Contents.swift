cb7e3245837509dcaee8945dcc33946e4f31c70f
Dennis <dnsljnv411@gmail.com>
2022-10-23 11:39:30 +0700

    Add interesting syntax

--- AdvancedSwift.playground/Pages/Some.xcplaygroundpage/Contents.swift
+++ AdvancedSwift.playground/Pages/Some.xcplaygroundpage/Contents.swift
@@ -1,9 +1,11 @@
 import Foundation
 
+// 1
 var array = [Int]()
 array.reserveCapacity(10)
 array.removeAll(keepingCapacity: true)
 
+// 2
 // Failable init can return nil
 struct FailabelStruct {
     let num: Int
@@ -20,4 +22,16 @@ struct FailabelStruct {
 // Dependent type cuz has another type inside
 struct DependentStruct {
     let dependency: FailabelStruct
+}
+
+// 3
+let someOptional: Int? = 42
+// Match using an enumeration case pattern.
+if case .some(let x) = someOptional {
+    print(x)
+}
+
+// Match using an optional pattern.
+if case let x? = someOptional {
+    print(x)
 }
--- AdvancedSwift.playground/Pages/Types.xcplaygroundpage/Contents.swift
+++ AdvancedSwift.playground/Pages/Types.xcplaygroundpage/Contents.swift
@@ -14,6 +14,7 @@ class EquatableClass: Equatable {
     init(id: Int, name: String) { self.id = id; self.name = name }
     
 // reference types do not automatically conform to Equatable to avoid reference cycles
+// and circular graph of objects and create an infitit loop of checking equality
     static func == (lhs: EquatableClass, rhs: EquatableClass) -> Bool {
         lhs.id == rhs.id && lhs.name == rhs.name
     }
@@ -23,6 +24,15 @@ class CodableClass: Codable {
     let id: Int
     let name: String
     
-// unlike Equatable reference types can conform to Codable and reate graph of referencd cycles
-    
+// unlike Equatable reference types can conform to Codable and create a graph of referenc cycles
 }
+
+// For an enum, all its associated values must conform to Equatable. (An enum without associated values has Equatable conformance even without the declaration.)
+
+typealias Country = String
+typealias Capital = String
+
+let country = Country("the USA")
+let capital = Capital("Washinkton")
+
+let list: [Country: Capital] = [country: capital]
