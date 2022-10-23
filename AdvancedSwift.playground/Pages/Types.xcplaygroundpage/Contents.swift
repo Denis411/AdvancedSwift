3f9af1bf8a97486d1bfd4c7b9b3a66d5cce9741c
Dennis <dnsljnv411@gmail.com>
2022-10-23 12:05:43 +0700

    Add comments

--- AdvancedSwift.playground/Pages/Types.xcplaygroundpage/Contents.swift
+++ AdvancedSwift.playground/Pages/Types.xcplaygroundpage/Contents.swift
@@ -14,7 +14,7 @@ class EquatableClass: Equatable {
     init(id: Int, name: String) { self.id = id; self.name = name }
     
 // reference types do not automatically conform to Equatable to avoid reference cycles
-// and circular graph of objects and create an infitit loop of checking equality
+// and circular graph of objects and create an infinit loop of checking equality
     static func == (lhs: EquatableClass, rhs: EquatableClass) -> Bool {
         lhs.id == rhs.id && lhs.name == rhs.name
     }
@@ -36,3 +36,33 @@ let country = Country("the USA")
 let capital = Capital("Washinkton")
 
 let list: [Country: Capital] = [country: capital]
+
+// Hashable
+// hash value is calculated by the means of modulo
+// hash value is used to get fast access to an element in a set or a dictionary
+// hash value is not random the same variables combined in a hasher give the same value
+// two different types with the same values give the same hash value
+// two different types with different values might give the same hash this is known as a collision
+//
+
+// structs automatically conform to both Equatable and Hashale
+struct HashableStruct: Hashable {
+    let num: Int
+}
+
+// classes do not automatically conform to either Hashable or Equatable
+// you have to conform to Equatable first to conform to Hashable
+// ⚠️ if objects are equal they MUST retuen the same hash value the reverse is not true 
+class HashableClass: Hashable {
+    var num: Int = 0
+    
+    static func == (lhs: HashableClass, rhs: HashableClass) -> Bool {
+//      a goog practice is to use the same vars in the hash func
+        lhs.num == rhs.num
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(num)
+    }
+}
+
